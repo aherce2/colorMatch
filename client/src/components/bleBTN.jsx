89b67daee89977254745ad3eca630d81b3d92f0b
aherce2 <aherce2@illinois.edu>
2025-04-10 14:22:38 -0500

    Handled BLE Connection with python Server -> Printing for now

--- client/src/components/bleBTN.jsx
+++ client/src/components/bleBTN.jsx
@@ -1,38 +1,28 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Toast from 'react-bootstrap/Toast';
+import useConnectBLE from '../connectBle'; 
 
 function ConnectBLE() {
-  const [bleStatus, setBleStatus] = useState(false); // State to track BLE connection status
-  const [showToast, setShowToast] = useState(false); // State to control toast visibility
-
-  // Function to handle BLE connection toggle
-  const toggleBleConnection = () => {
-    const newStatus = !bleStatus;
-    setBleStatus(newStatus);
-    setShowToast(newStatus); // Show toast only when connected
-  };
-  useEffect(() => {
-    let timer;
-    if (showToast) {
-      timer = setTimeout(() => {
-        setShowToast(false);
-      }, 2500); 
-    }
-    return () => clearTimeout(timer); // Cleanup timeout on unmount or state change
-  }, [showToast]);
+  
+  const { 
+    handleOnClick, 
+    buttonText, 
+    showToast, 
+    toastText, 
+    setShowToast 
+  } = useConnectBLE(); // Custom Hook
 
   return (
     <div className="d-grid gap-2">
-      <Button onClick={toggleBleConnection} className="me-3">
-        {bleStatus ? 'Disconnect Device' : 'Connect to Device'}
+      <Button onClick={handleOnClick} className="me-3">
+        {buttonText}
       </Button>
       {showToast && (
         <Toast onClose={() => setShowToast(false)} show={showToast} animation={false}>
           <Toast.Header>
-            <Toast.Body>Successfully connected to the device!</Toast.Body>
+            <strong className="me-auto">{toastText}</strong>
           </Toast.Header>
-          
         </Toast>
       )}
     </div>
@@ -40,30 +30,3 @@ function ConnectBLE() {
 }
 
 export default ConnectBLE;
-
-
-  
-// function ConnectBLE() {
-//   const [bleStatus, setBleStatus] = useState(false); // State to track BLE connection status
-
-//   // Function to handle button press -> Modify for axios BLE Connection
-//   const handleButtonClick = () => {
-//     setBleStatus(!bleStatus); // Toggle the BLE status
-//   };
-
-//   // Determine the text to display based on bleStatus
-//   const buttonVariant = bleStatus ? 'primary' : 'outline-primary';
-//   const buttonText = bleStatus
-//     ? 'Connected to Device'
-//     : 'Press to Connect to Device';
-
-//   return (
-//     <div className="d-grid gap-2">
-//       <Button variant={buttonVariant} size="lg" onClick={handleButtonClick}>
-//         {buttonText}
-//       </Button>
-//     </div>
-//   );
-// }
-
-// export default ConnectBLE;
--- server/main.py
+++ server/main.py
@@ -1,30 +1,33 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
-from ble import get_ble
+
 # Create An App Instance
 app = Flask(__name__)
 
-# Enable Orgins -> Accept all origins for now
+# Enable Origins -> Accept all origins for now
 cors = CORS(app, origins='*')
 
-@app.route("/api/users", methods=['GET'])
-def users():
-    return jsonify(
-        {
-            "users": [
-                'Ashley',
-                'Angel'   
-            ]
-            
-        }
-    )
-
-
 # Modify to establish BLE and wait
-@app.route("/api/ble", methods=['POST'])
-def bluetooth_connection():
-    return get_ble()
+@app.route("/api/ble/<status>", methods=['GET'])
+def bluetooth_connection(status):
 
+    # Device is connected -> Disconnect from Device
+    if status.lower() == 'true':
+        return jsonify({
+            "success": True,
+            "message": "Disconnected from BLE Device"
+        })
+    # Device is disconnected -> Connect to Device
+    elif status.lower() == 'false':
+        return jsonify({
+            "success": True,
+            "message": "Connected to BLE Device"
+        })
+    else:
+        return jsonify({
+            "success": False,
+            "message": "Invalid status provided"
+        }), 400
 
 
 if __name__ == "__main__":
