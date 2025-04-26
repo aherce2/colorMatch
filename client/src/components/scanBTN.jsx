e85ee6da964f2b70555865dd13f5b18217af4102
aherce2 <aherce2@illinois.edu>
2025-04-26 12:44:50 -0500

    need to fix message ack when button is pressed

--- client/src/components/scanBTN.jsx
+++ client/src/components/scanBTN.jsx
@@ -5,8 +5,8 @@ const ScanButton = ({ scanStatus, onStartScan,command  }) => {
     <div className="d-grid gap-2 my-3">
       {/* <button className="btn btn-secondary" onClick={onStartScan}> */}
       <button className="btn btn-secondary" onClick={() => onStartScan(command)}>
-        {command === '1' ? 'Start Scan' : 'Stop Scan'}
-        {/* {scanStatus === 'scanning' ? 'Scanning...' : 'One Shot Scan'} */}
+        {command === '1'}
+        {scanStatus === 'scanning' ? 'Scanning...' : 'One Shot Scan'}
       </button>
     </div>
   );
--- server/communicationBLE.py
+++ server/communicationBLE.py
@@ -17,12 +17,23 @@ def on_notification(data: bytes):
     
     message = data.decode().strip()
     if message.startswith("ACK:"):
+        # ack_command = message[4:]
+        # if ack_command == "start":
+        #     constants.socketio.emit('scan_status', {
+        #         'status': 'acknowledged',
+        #         'message': 'ESP32 received start command'
         ack_command = message[4:]
-        if ack_command == "start":
-            constants.socketio.emit('scan_status', {
+        if ack_command == "1":
+            print("ACK ACKNOWLEDGED")
+            socketio.emit('scan_status', {
                 'status': 'acknowledged',
-                'message': 'ESP32 received start command'
+                'message': 'ESP Recieved Command'
             })
+        elif ack_command == "0":
+            socketio.emit('scan_status', {
+                'status': 'acknowledged',
+                'message': 'Scan stopped'
+        })
     else:
         header = data[0]
         payload = data[1:]
@@ -37,38 +48,4 @@ def on_notification(data: bytes):
                 print("Invalid LAB payload")
         else:
             print(f"Unknown header: {hex(header)}")
-        
-# def send_message(message):
-#     global peripheral_instance
-#     try:
-#         # Add connection checks
-#         if not peripheral_instance:
-#             logging.error("No BLE device reference")
-#             return False
-            
-#         if not peripheral_instance or not peripheral_instance.is_connected():
-#             logging.error("Device not connected")
-#             return False
-            
-#         # Convert to byte array if needed
-#         if isinstance(message, str):
-#             data = message.encode()
-#         else:
-#             data = message
-            
-#         # peripheral_instance.write_request(
-#         #     constants.SERVICE_UUID,
-#         #     constants.CHARACTERISTIC_UUID,
-#         #     data
-#         # )
-#         peripheral_instance.write_command(
-#             constants.SERVICE_UUID,
-#             constants.CHARACTERISTIC_UUID,
-#             data
-#         )
-
-#         logging.info(f"Sent: {message}")
-#         return True
-#     except Exception as e:
-#         logging.error(f"Send error: {e}")
-#         return False
+    
